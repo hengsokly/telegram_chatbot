8f5d6b445a4b201f9d4f1eb3c0236d2290bf0525
kakada <kakada.chheang@gmail.com>
2021-04-30 10:51:32 +0700

    Hotfix misspelling variable name in notification worker

--- app/workers/telegram_chatbot/notification_worker.rb
+++ app/workers/telegram_chatbot/notification_worker.rb
@@ -7,7 +7,7 @@ module TelegramChatbot
     def perform(message, group_id = nil)
       return if !(group_id.present? && message.present?)
 
-      Notification.notify(message, group)
+      Notification.notify(message, group_id)
     end
   end
 end
